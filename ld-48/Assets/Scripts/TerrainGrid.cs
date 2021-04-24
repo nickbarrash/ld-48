423804d805235a8f689f38c621c21e44f960a244
Nick Barrash <nickbarrash@gmail.com>
2021-04-23 22:26:56 -0400

    0.0.0 render grid basic value setup

---
+++ ld-48/Assets/Scripts/TerrainGrid.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGrid : MonoBehaviour
+{
+    public int GRID_X = 30;
+    public int GRID_Y = 300;
+
+    public bool debugMode = false;
+    public GameObject terrainPrefab;
+    public TerrainSquare[,] grid;
+
+    public TerrainSquare getSquare(int x, int y) {
+        if (x < 0 || x >= grid.GetLength(0) || y < 0 || y > grid.GetLength(1)) {
+            return null;
+        }
+        return grid[x, y];
+    }
+
+    public void generate() {
+        TerrainSquare.TERRAIN_TYPE[] type = new TerrainSquare.TERRAIN_TYPE[] {
+            TerrainSquare.TERRAIN_TYPE.EMPTY,
+            TerrainSquare.TERRAIN_TYPE.DENSE_ROCK,
+            TerrainSquare.TERRAIN_TYPE.GAS_POCKET,
+            TerrainSquare.TERRAIN_TYPE.COPPER,
+            TerrainSquare.TERRAIN_TYPE.SILVER,
+            TerrainSquare.TERRAIN_TYPE.GOLD,
+            TerrainSquare.TERRAIN_TYPE.PLATINUM,
+            TerrainSquare.TERRAIN_TYPE.DIAMOND,
+        };
+
+        grid = new TerrainSquare[GRID_X, GRID_Y];
+
+        for(int x = 0; x < grid.GetLength(0); x++) {
+            for (int y = 0; y < grid.GetLength(0); y++) {
+                GameObject newObj = Instantiate(terrainPrefab, transform);
+                newObj.name = $"TerrainSquare_{x}_{y}";
+                grid[x, y] = newObj.GetComponent<TerrainSquare>();
+                grid[x, y].grid = this;
+                grid[x, y].spawn(type[Random.Range(0, type.Length)], x, y);
+                grid[x, y].transform.position = new Vector3(x, y * -1, 0);
+            }
+        }
+    }
+
+    private void Start() {
+        generate();
+    }
+}
---
+++ ld-48/Assets/Scripts/TerrainSquare.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TerrainSquare : MonoBehaviour
+{
+    public enum STATE {
+        EXCAVATED,
+        UNEXCAVATED
+    }
+
+    public enum TERRAIN_TYPE {
+        EMPTY = 0,
+        DENSE_ROCK = -7,
+        GAS_POCKET = -13,
+        COPPER = 2,
+        SILVER = 5,
+        GOLD = 10,
+        PLATINUM = 20,
+        DIAMOND = 100
+    }
+
+    public TerrainGrid grid;
+    public int x;
+    public int y;
+    int displayValue;
+    public TERRAIN_TYPE type;
+    public STATE state;
+
+    public TMP_Text valueText;
+    public GameObject excavated;
+    public GameObject unexcavated;
+
+    public IEnumerable<TerrainSquare> neighbors() {
+        for (int xOff = -1; xOff <= 1; xOff++) {
+            for (int yOff = -1; yOff <= 1; yOff++) {
+                if (xOff != 0 || yOff != 0) {
+                    var neighbor = grid.getSquare(x + xOff, y + yOff);
+                    if (neighbor != null) {
+                        yield return neighbor;
+                    }
+                }
+            }
+        }
+    }
+
+    public int getValue() {
+        return state == STATE.EXCAVATED ? 0 : (int)type;
+    }
+
+    public void setAffordances() {
+        if (grid.debugMode) {
+            valueText.text = $"{getValue()}";
+        } else {
+            valueText.text = $"NYI";
+        }
+
+        setExcavatedAffordances(state == STATE.EXCAVATED);
+    }
+
+    private void setExcavatedAffordances(bool isExcavated) {
+        excavated.SetActive(isExcavated);
+        unexcavated.SetActive(!isExcavated);
+    }
+
+    public void setState(STATE newState) {
+        state = newState;
+        setAffordances();
+    }
+
+    public void spawn(TERRAIN_TYPE newType, int x, int y) {
+        this.x = x;
+        this.y = y;
+        type = newType;
+        setState(STATE.UNEXCAVATED);
+    }
+}
