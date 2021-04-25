2fdcb788f0ba21dd4b73aeb49d74bd8512587023
Nick Barrash <nickbarrash@gmail.com>
2021-04-25 00:04:49 -0400

    purchasing seems to work :O

--- ld-48/Assets/Scripts/UI/StoreDisplay.cs
+++ ld-48/Assets/Scripts/UI/StoreDisplay.cs
@@ -6,7 +6,7 @@ public class StoreDisplay : MonoBehaviour
 {
     int BOMB_PRICE = 1000;
     int SCAN_PRICE = 200;
-    int REPAIR_PRICE = 10;
+    int REPAIR_PRICE = 25;
     int HULL_UPGRADE_PRICE = 150;
     int EARTHQUAKE_PRICE = 16000;
     int LIQUIFACTION_PRICE = 5000;
@@ -22,10 +22,18 @@ public class StoreDisplay : MonoBehaviour
         gameObject.SetActive(isVisible);
     }
 
-    //public void buyGas() {
-    //    if (GameManager.instance.player.money > GAS_PRICE) {
-    //        GameManager.instance.player.updateGas(20);
-    //        GameManager.instance.player.updateMoney(-1 * GAS_PRICE);
-    //    }
-    //}
+    public void buyRepair() {
+        if (GameManager.instance.player.money > REPAIR_PRICE && GameManager.instance.player.health < GameManager.instance.player.maxHealth) {
+            GameManager.instance.player.updateHealth(20);
+            GameManager.instance.player.updateMoney(-1 * REPAIR_PRICE);
+        }
+    }
+
+    public void buyHullUpgrade() {
+        if (GameManager.instance.player.money > HULL_UPGRADE_PRICE) {
+            GameManager.instance.player.updateHealth(20);
+            GameManager.instance.player.maxHealth += 20;
+            GameManager.instance.player.updateMoney(-1 * HULL_UPGRADE_PRICE);
+        }
+    }
 }
